6c1607e0c16479926476f10eaead28099969d366
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-22 17:21:26 +0100

    added routes in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() //Get all users
+  findAll() {
+    return 'This action returns all users';
+  }
+
+  @Get(':id') //Get user by id
+  findOne() {
+    return 'This action returns a user by id';
+  }
+
+  @Get('interns') //Get all interns
+  findAllInterns() {
+    return 'This action returns all interns';
+  }
+}
