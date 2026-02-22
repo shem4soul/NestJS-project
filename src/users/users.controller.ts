5f7b6ba25ca093ac4cda0f2c823649916696bc34
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-22 17:43:43 +0100

    fix a bug in users.controller.ts by adding missing parameters to the findOne and update methods.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -7,13 +15,28 @@ export class UsersController {
     return 'This action returns all users';
   }
 
-  @Get('interns') //Get all interns
-  findAllInterns() {
-    return [];
-  }
+  //   @Get('interns') //Get all interns
+  //   findAllInterns() {
+  //     return [];
+  //   }
 
   @Get(':id') //Get user by id
-  findOne() {
+  findOne(@Param('id') id: string) {
+    return 'This action returns a user by id';
+  }
+
+  @Post() //Create a new user
+  create(@Body() user: {}) {
+    return 'This action creates a new user';
+  }
+
+  @Patch(':id') //PATCH / users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //DELETE /users/:id
+  delete(@Param('id') id: string) {
     return 'This action returns a user by id';
   }
 }
