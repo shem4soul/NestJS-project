16c7695e016511021cab717e2cbc88421d5e5122
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-22 18:14:59 +0100

    added a list of users to the UsersService class

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      email: 'jane@fastermail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Admin User',
+      email: ' admin@code.uba.uk',
+      role: 'ADMIN',
+    },
+    {
+      id: 4,
+      name: 'Alice Smith',
+      email: 'smith@hotmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Bob Johnson',
+      email: 'johnson@yahoo.co.uk',
+      role: 'INTERN',
+    },
+  ];
+}
