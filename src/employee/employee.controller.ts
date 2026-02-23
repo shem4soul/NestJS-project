b9f5f1cb87fb70a27a3a565dd98871b7c06321ab
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-23 10:21:11 +0100

    fix employee controller to use Prisma DTOs

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
-import { CreateEmployeeDto } from './dto/create-employee.dto';
-import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { Prisma } from '@prisma/client';
 
 @Controller('employee')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeeService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll() {
-    return this.employeeService.findAll();
+  findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
+    return this.employeeService.findAll(role);
   }
 
   @Get(':id')
@@ -23,7 +31,10 @@ export class EmployeeController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
+  ) {
     return this.employeeService.update(+id, updateEmployeeDto);
   }
 
