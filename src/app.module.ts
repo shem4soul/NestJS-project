68bf3ff9e0e30d8afe388b78acc35ba69f633dc0
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-23 10:10:49 +0100

    added employee module with controller and service files, and updated app.module.ts to include employee module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
