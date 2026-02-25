d2a31c409f2e9e01a03948d69d739a6eb0c24967
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-25 02:33:16 +0100

    installed  config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,18 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { UsersModule } from './users/users.module';
+// import { DatabaseModule } from './database/database.module';
+// import { EmployeeModule } from './employee/employee.module';
+
+// @Module({
+//   imports: [UsersModule, DatabaseModule, EmployeeModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // ✅ ADD THIS
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,7 +20,15 @@ import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeeModule],
+  imports: [
+    ConfigModule.forRoot({
+      // ✅ THIS LOADS .env
+      isGlobal: true,
+    }),
+    UsersModule,
+    DatabaseModule,
+    EmployeeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,8 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaNeon({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({
+      adapter,
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
